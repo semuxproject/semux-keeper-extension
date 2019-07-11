66286930bcbd020f49e1c136347a556d1668d470
honeycrypto <honeycrypto@tuta.io>
2019-07-11 14:01:42 +0200

    UI: Show votes in delegates list

--- js/fillValidators.js
+++ js/fillValidators.js
@@ -3,26 +3,20 @@
 
 const API_DELEGATES = 'https://api.testnet.semux.online/v2.2.0/delegates'
 
-async function fillDelegates () {
+async function getDelegates () {
   const data = await fetch(API_DELEGATES)
   const json = await data.json()
   if (json.success) {
-    var html = ''
+    let html = ''
     const delegates = json.result
     for (let i = 0; i < delegates.length; i++) {
-      html += `<option data-address=${delegates[i]['address']}>${delegates[i]['name']} (${formatAddress(delegates[i]['address'])})</option>`
+      html += `<option data-address="${delegates[i]['address']}" ` +
+      `data-subtext="${parseInt(parseFloat(delegates[i]['votes']) / 1e9, 10)} SEM">` +
+      `${delegates[i]['name']}</option>`
     }
     $('select.delegatesList').html(html)
     $('select.delegatesList.liveSearch').selectpicker()
   }
 }
 
-fillDelegates()
-
-function formatAddress (address) {
-  // first 6 and last 4 symbols
-  const first = address.substring(0, 5)
-  const last = address.substring(address.length - 3, address.length)
-  const result = first + '...' + last
-  return result
-}
+getDelegates()
